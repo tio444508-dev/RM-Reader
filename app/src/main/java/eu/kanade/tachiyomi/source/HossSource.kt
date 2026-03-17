89d457bfe0908d2885cd258fce3eba11c2ee901c
tio444508-dev <tio444508@gmail.com>
2026-03-16 22:13:22 -0300

    Create HossSource.kt

---
+++ app/src/main/java/eu/kanade/tachiyomi/source/HossSource.kt
@@ -0,0 +1,16 @@
+package com.rmreader.source
+
+import org.jsoup.nodes.Document
+import com.rmreader.model.Page
+
+class HossSource {
+    val baseUrl = "https://hossmanhwa.com/"
+
+    // Esse código limpa o site e pega só as fotos das páginas
+    fun extrairPaginas(document: Document): List<Page> {
+        return document.select(".reading-content img").mapIndexed { index, element ->
+            val urlImagem = element.attr("abs:data-src").ifEmpty { element.attr("abs:src") }
+            Page(index, urlImagem)
+        }
+    }
+}

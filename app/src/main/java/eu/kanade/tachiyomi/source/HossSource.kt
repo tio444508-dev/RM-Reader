c2b48f4811e23ec9b86ee10ebd4e9be5ea9a1030
tio444508-dev <tio444508@gmail.com>
2026-03-16 23:14:29 -0300

    test 6

--- app/src/main/java/eu/kanade/tachiyomi/source/HossSource.kt
+++ app/src/main/java/eu/kanade/tachiyomi/source/HossSource.kt
@@ -17,7 +17,7 @@ class HossSource : HttpSource() {
     override val lang = "pt-BR"
     override val supportsLatest = true
 
-    // Configurações básicas para o Android aceitar o arquivo
+    // Funções obrigatórias para o motor do app aceitar o arquivo
     override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/manga-list/", headers)
     override fun popularMangaParse(response: Response): MangasPage = MangasPage(emptyList(), false)
     override fun latestUpdatesRequest(page: Int): Request = GET(baseUrl, headers)
@@ -29,7 +29,7 @@ class HossSource : HttpSource() {
     override fun pageListParse(response: Response): List<Page> = emptyList()
     override fun imageUrlParse(response: Response): String = ""
 
-    // Sua lógica de extração
+    // Sua lógica de extração que vamos usar no teste
     fun extrairPaginas(document: Document): List<Page> {
         return document.select(".reading-content img").mapIndexed { index, element ->
             val urlImagem = element.attr("abs:data-src").ifEmpty { element.attr("abs:src") }
